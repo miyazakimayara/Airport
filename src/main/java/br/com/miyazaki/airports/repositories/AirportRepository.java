8e8d528fcb1ffd8f790ca183188a6f02c821d26b
Mayara Miyazaki <mayaramiyazakipng.senai@gmail.com>
2025-05-21 15:42:25 -0300

    Término da Fase 2 –Implementado endpoint Airport -Add interface AirportRepository -Add classe AirportService -Add classe AirportController

---
+++ src/main/java/br/com/miyazaki/airports/repositories/AirportRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.com.miyazaki.airports.repositories;
+
+import br.com.miyazaki.airports.entities.Airport;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author ppjatb
+ */
+public interface AirportRepository extends JpaRepository<Airport, Long>{
+    
+}
---
+++ src/main/java/br/com/miyazaki/airports/service/AirportService.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.miyazaki.airports.service;
+
+import br.com.miyazaki.airports.entities.Airport;
+import br.com.miyazaki.airports.repositories.AirportRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author ppjatb
+ */
+@Service
+public class AirportService {
+    @Autowired
+    private AirportRepository airportRepository;
+    
+    public List<Airport> findAll(){
+        
+        List<Airport> result = airportRepository.findAll();
+        return result;
+    }
+}
+
